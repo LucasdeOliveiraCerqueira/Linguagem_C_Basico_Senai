ea7e4a8691ddb26e99bb18d1a9d561b857a84b9d
Lucas Cerqueira <lls072025@gmail.com>
2025-09-04 22:36:15 -0300

    Add files via upload

---
+++ desafio1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//entrada e saida de dados
+#include <stdlib.h>
+//Funções no geral ( conversões, memoria, comandos do sistema) tipo o sisteam("clear"); = limpa()
+#include <math.h>
+// matemática no geral
+#include <string.h>
+// trabalhar textos
+#include <time.h>
+// relogio, contagem de tempo...
+#include <ctype.h>
+// testar e converter letras/números
+int main(){
+		float number;
+		int i;
+		
+		for(i = 1;i <= 5;i++){
+			printf("Digite um numero: ");
+			scanf("%f", &number);
+			system("cls");   
+		}   
+	}
+		
